9979cb2e8c2d7bc951b888ed88e8b532898e4337
ibrahimZ91 <ibrahim.zeidan.eng@gmail.com>
2024-01-21 20:38:18 +0100

    added from mac

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Running for one more time!');
+  res.send('Running....Mac');
 });
 
 app.listen(PORT, HOST);
